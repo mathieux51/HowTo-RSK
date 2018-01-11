a89823d28b3448830b0840e07396923432685da8
Mathieu <mathieux51@gmail.com>
2018-01-11 10:50:14 +0100

    Fix bug

--- src/handlers/fileHandler.js
+++ src/handlers/fileHandler.js
@@ -3,13 +3,15 @@ import fs from 'fs';
 import { v4 } from 'uuid';
 
 export default function fileHandler(req, res, next) {
-  const destination = 'public/gifs/';
-  res.locals.fileId = v4();
-  res.locals.fileName = res.locals.fileId + req.file.detectedFileExtension;
-  const finalPath = path.join(destination, res.locals.fileName);
-  const outStream = fs.createWriteStream(finalPath);
+  if (req.file.size) {
+    const destination = 'public/gifs/';
+    res.locals.fileId = v4();
+    res.locals.fileName = res.locals.fileId + req.file.detectedFileExtension;
+    const finalPath = path.join(destination, res.locals.fileName);
+    const outStream = fs.createWriteStream(finalPath);
 
-  req.file.stream.pipe(outStream);
-  outStream.on('error', next);
-  outStream.on('finish', next);
+    req.file.stream.pipe(outStream);
+    outStream.on('error', next);
+    outStream.on('finish', next);
+  }
 }
